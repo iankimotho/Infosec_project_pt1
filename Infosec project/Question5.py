49d29f085c12ef5705cf7b7fc72417400aef2b39
Kimotho, Ian Muchai <Ian.Kimotho@strathmore.edu>
2025-05-26 22:46:25 +0300

    Delete Infosec project directory

--- Infosec project/Question5.py
+++
@@ -1,8 +0,0 @@
-x = 0
-y = 20
-
-while y >= 6:
-    y = y - 4
-    x = x + (2 / y)
-
-print("x: ",x)
--- Infosec project/Question6.py
+++
@@ -1,8 +0,0 @@
-values = []
-
-for i in range(5):
-    val = float(input(f"Enter the value {i+1}: "))
-    values.append(val)
-
-average = sum(values) / 5
-print("Average: ", average)
